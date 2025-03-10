02f5d33b213e7bf8629ab967bb1e6729282b0e44
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-03-10 12:33:06 +0600

    firebasse start

--- my-project/src/component/Register.jsx
+++ my-project/src/component/Register.jsx
@@ -1,9 +1,18 @@
 
 
 const Register = () => {
+
+    const handleRegister=(event)=>{
+        event.preventDefault();
+        const email=event.target.email.value;
+        const password=event.target.password.value;
+        console.log(email,password);
+    }
+
+
     return (
         <div className="max-w-xl mx-auto ">
-            <form >
+            <form onSubmit={handleRegister}>
                 <label className="input input-bordered flex items-center gap-2 my-6">
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
@@ -15,8 +24,9 @@ const Register = () => {
                         <path
                             d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
                     </svg>
-                    <input type="text" className="grow" placeholder="Email" />
+                    <input type="email" name="email" className="grow" placeholder="Email" />
                 </label>
+
                 <label className="input input-bordered flex items-center gap-2 my-6">
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
@@ -28,7 +38,7 @@ const Register = () => {
                             d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
                             clipRule="evenodd" />
                     </svg>
-                    <input type="password" className="grow" value="password" />
+                    <input type="password" name="password" className="grow" placeholder="password" />
                 </label>
                 <button className="btn btn-accent">Accent</button>
             </form>
