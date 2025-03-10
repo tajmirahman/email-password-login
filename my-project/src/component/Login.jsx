5eb73de14a2bc18b2479d0fc9f83ad8178e94e89
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-03-10 19:06:43 +0600

    login form start

--- my-project/src/component/Login.jsx
+++ my-project/src/component/Login.jsx
@@ -1,9 +1,50 @@
+import { signInWithEmailAndPassword } from "firebase/auth";
+import auth from "../firebase.init";
 
 
 const Login = () => {
+
+    const handleLoginForm=e=>{
+        e.preventDefault();
+        const email=e.target.email.value;
+        const password=e.target.password.value;
+
+        console.log(email, password);
+
+        signInWithEmailAndPassword(auth,email,password)
+        .then(result=>{
+            console.log(result.user);
+        })
+        .catch(error=>{
+            console.log('Error', error.message);
+        })
+
+
+    }
+
     return (
-        <div>
-            <h1>Login page</h1>
+        <div className="hero bg-base-200 min-h-screen">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-5xl font-bold">Login now!</h1>
+                    <p className="py-6">
+                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                        quasi. In deleniti eaque aut repudiandae et a id nisi.
+                    </p>
+                </div>
+                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                    <form onSubmit={handleLoginForm} className="card-body">
+                        <fieldset className="fieldset">
+                            <label className="fieldset-label">Email</label>
+                            <input type="email" name="email" className="input" placeholder="Email" />
+                            <label className="fieldset-label">Password</label>
+                            <input type="password" name="password" className="input" placeholder="Password" />
+                            <div><a className="link link-hover">Forgot password?</a></div>
+                            <button className="btn btn-neutral mt-4">Login</button>
+                        </fieldset>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
